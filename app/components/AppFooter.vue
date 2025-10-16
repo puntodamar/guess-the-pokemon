2c2a47db1e19dca5ebc1145fb0ad84c282eee335
puntodamar <punto.damar2@gmail.com>
2025-10-17 03:05:22 +0700

    update: footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="mx-auto text-xs opacity-50 m-5">Made with Pokemon API <NuxtLink to="https://pokeapi.co/" target="_blank">https://pokeapi.co/</NuxtLink></div>
+  <div class="mx-auto text-xs opacity-50 m-5">
+    Made by <NuxtLink to="https://puntodamar.vercel.app/" class="text-pokemon-heading" target="_blank">puntodamar</NuxtLink> with <NuxtLink class="text-pokemon-heading" to="https://pokeapi.co/" target="_blank">https://pokeapi.co/</NuxtLink>
+  </div>
 </template>
