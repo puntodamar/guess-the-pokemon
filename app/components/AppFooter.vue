2a891648712b06db1be191ba2f09df1bd8d89a83
puntodamar <punto.damar2@gmail.com>
2025-10-17 00:54:08 +0700

    update: footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>AppFooter</div>
+  <div class="mx-auto text-xs opacity-50 m-5">Made with Pokemon API <NuxtLink to="https://pokeapi.co/" target="_blank">https://pokeapi.co/</NuxtLink></div>
 </template>
